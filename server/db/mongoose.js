e2c15b97f92f7bfb5396c6a7eb7ef594a8dcee9f
Vibhu Mishra <31285739+Mishra121@users.noreply.github.com>
2018-02-25 19:36:47 +0530

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 // mongoose make validation and many functionalities simpler
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
   mongoose
